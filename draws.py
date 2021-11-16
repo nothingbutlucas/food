4f6dd8059c11c25cace33adc8cb3f18d74c634b1
Lucas <blanco.lucas@outlook.com>
2021-11-16 19:00:50 -0300

    Menos dibujos que no messervian

---
+++ draws.py
@@ -0,0 +1,22 @@
+def new_line():
+	print("\n")
+
+def rat():
+	print("  ,,==.                      (bai)")
+	print(" //    `                      /")
+	print("||      ,--~~~~-._ _(\--,_   /")
+	print(" \\._,-~   \      '    *  `o ")
+	print("  `---~\( _/,___( /_/`---~~")
+	print("        ``==-    `==-,")
+
+
+def printbarra():
+	print("="*50)
+
+
+def printequis():
+	print("Xx"*30)
+
+
+def printflorcitas():
+	print("*"*50)
